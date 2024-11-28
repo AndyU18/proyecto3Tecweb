e762ccf636dede114bb536951735cf85a0aa9511
AndyU18 <andres.urquidi@ucb.edu.bo>
2024-11-28 00:02:07 -0400

    mejoras de acerca de nosotros

--- proyecto3_react_vue/src/pages/AboutUs.jsx
+++ proyecto3_react_vue/src/pages/AboutUs.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import Slider from 'react-slick'; // Importando el slider de react-slick
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const AboutUs = () => {
+  const [inView, setInView] = useState(false);
+
   const brands = [
-    "https://via.placeholder.com/200x100?text=Marca+1",
-    "https://via.placeholder.com/200x100?text=Marca+2",
-    "https://via.placeholder.com/200x100?text=Marca+3",
-    "https://via.placeholder.com/200x100?text=Marca+4",
+    "/marcas/adidas.jpg",
+    "/marcas/joma.jpg",
+    "/marcas/lacoste.png",
+    "/marcas/nike.jpg",
+    "/marcas/north.png",
+    "/marcas/puma.jpg",
   ];
 
   const settings = {
@@ -21,6 +25,26 @@ const AboutUs = () => {
     autoplay: true,
     autoplaySpeed: 3000,
   };
+
+  // Verifica si la sección está en vista
+  const checkInView = () => {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        setInView(true); // Marcar como visible
+      } else {
+        setInView(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkInView);
+    return () => {
+      window.removeEventListener('scroll', checkInView);
+    };
+  }, []);
 
   return (
     <div style={styles.pageContainer}>
@@ -38,9 +62,10 @@ const AboutUs = () => {
         {/* Historia de la tienda */}
         <motion.section
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          animate={{ opacity: inView ? 1 : 0 }} // Desvanecerse al entrar en vista
           transition={{ duration: 1 }}
           style={styles.historySection}
+          className="section" // Añadir clase para identificar la sección
         >
           <h2>Historia</h2>
           <p>
@@ -51,9 +76,10 @@ const AboutUs = () => {
         {/* Misión y Visión */}
         <motion.section
           initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          animate={{ opacity: inView ? 1 : 0 }} // Desvanecerse al entrar en vista
           transition={{ duration: 1 }}
           style={styles.missionVisionSection}
+          className="section" // Añadir clase para identificar la sección
         >
           <div style={styles.missionVisionContainer}>
             <div style={styles.mission}>
@@ -82,7 +108,12 @@ const AboutUs = () => {
           <Slider {...settings}>
             {brands.map((brand, index) => (
               <div key={index} style={styles.sliderItem}>
-                <img src={brand} alt={`Marca ${index + 1}`} style={styles.brandImage} />
+                <img
+                  src={brand}
+                  alt={`Marca ${index + 1}`}
+                  style={styles.brandImage}
+                  onLoad={() => console.log(`Imagen ${index + 1} cargada`)}
+                />
               </div>
             ))}
           </Slider>
@@ -109,96 +140,101 @@ const AboutUs = () => {
 };
 
 const styles = {
-    pageContainer: {
-      backgroundColor: '#f4f4f9', // Fondo claro
-      padding: '2rem',
-      minHeight: '100vh',
-      fontFamily: '"Roboto Condensed", sans-serif', // Fuente para el texto
-    },
-    header: {
-      backgroundColor: '#003366', // Fondo oscuro para el encabezado
-      color: '#fff', // Texto blanco para el contraste
-      padding: '4rem 2rem',
-      textAlign: 'center',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      fontFamily: '"Sixtyfour Convergence", sans-serif', // Fuente para los títulos
-    },
-    title: {
-      fontSize: '3rem', // Tamaño de fuente más grande
-      marginBottom: '0.5rem',
-      fontWeight: 'bold',
-    },
-    subtitle: {
-      fontSize: '1.5rem', // Subtítulo con fuente mayor
-      fontStyle: 'italic',
-    },
-    container: {
-      marginTop: '2rem',
-    },
-    historySection: {
-      marginBottom: '2rem',
-      backgroundColor: '#ffffff', // Fondo blanco para secciones internas
-      padding: '1.5rem',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      color: '#333', // Texto oscuro para legibilidad
-    },
-    missionVisionSection: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      gap: '2rem',
-      marginBottom: '2rem',
-    },
-    missionVisionContainer: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      gap: '1rem',
-    },
-    mission: {
-      flex: 1,
-      backgroundColor: '#f9f9f9', // Fondo gris claro
-      padding: '1.5rem',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      color: '#333',
-    },
-    vision: {
-      flex: 1,
-      backgroundColor: '#f9f9f9',
-      padding: '1.5rem',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      color: '#333',
-    },
-    brandsSection: {
-      marginBottom: '2rem',
-    },
-    sliderItem: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    brandImage: {
-      width: '100%',
-      height: 'auto',
-      borderRadius: '8px',
-    },
-    testimonialsSection: {
-      backgroundColor: '#ffffff', // Fondo blanco para testimonios
-      padding: '2rem',
-      borderRadius: '8px',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      color: '#333',
-    },
-    testimonial: {
-      marginBottom: '1rem',
-      fontStyle: 'italic',
-    },
-    testimonialAuthor: {
-      fontWeight: 'bold',
-    },
-  };
-  
-  export default AboutUs;
-  
+  pageContainer: {
+    backgroundColor: '#f4f4f9', // Fondo claro
+    padding: '2rem',
+    minHeight: '100vh',
+    fontFamily: '"Roboto Condensed", sans-serif', // Fuente para el texto
+  },
+  header: {
+    backgroundColor: '#003366', // Fondo oscuro para el encabezado
+    color: '#fff', // Texto blanco para el contraste
+    padding: '4rem 2rem',
+    textAlign: 'center',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    fontFamily: '"Lato", sans-serif', // Fuente para los títulos
+  },
+  title: {
+    fontSize: '3rem', // Tamaño de fuente más grande
+    marginBottom: '0.5rem',
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: '1.5rem', // Subtítulo con fuente mayor
+    fontStyle: 'italic',
+  },
+  container: {
+    marginTop: '2rem',
+  },
+  historySection: {
+    marginBottom: '2rem',
+    backgroundColor: '#ffffff', // Fondo blanco para secciones internas
+    padding: '1.5rem',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    color: '#333', // Texto oscuro para legibilidad
+  },
+  missionVisionSection: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '2rem',
+    marginBottom: '2rem',
+  },
+  missionVisionContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '1rem',
+  },
+  mission: {
+    flex: 1,
+    backgroundColor: '#f9f9f9', // Fondo gris claro
+    padding: '1.5rem',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    color: '#333',
+  },
+  vision: {
+    flex: 1,
+    backgroundColor: '#f9f9f9',
+    padding: '1.5rem',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    color: '#333',
+  },
+  brandsSection: {
+    marginBottom: '2rem',
+  },
+  sliderItem: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  brandImage: {
+    width: '200px',
+    height: '100px',
+    objectFit: 'contain', 
+    borderRadius: '8px',
+  },
+  testimonialsSection: {
+    backgroundColor: '#ffffff', // Fondo blanco para testimonios
+    padding: '2rem',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    color: '#333',
+  },
+  testimonial: {
+    marginBottom: '1rem',
+    fontStyle: 'italic',
+  },
+  testimonialAuthor: {
+    fontWeight: 'bold',
+  },
+  p:{
+    textAlign: 'center',
+    color: '#333',
+    fontSize: '1.5rem',
+  }
+};
+
+export default AboutUs;
