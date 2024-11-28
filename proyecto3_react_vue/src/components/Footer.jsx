e1db5bb952c13b0efac1d6adb99c060fcd558313
AndyU18 <andres.urquidi@ucb.edu.bo>
2024-11-27 21:46:28 -0400

    ruteo, mejora del inicio/landing page

---
+++ proyecto3_react_vue/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={styles.footer}>
+      <p>© 2024 Tu Compañía. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+const styles = {
+  footer: {
+    backgroundColor: '#333', // Fondo oscuro para contrastar
+    color: '#fff', // Texto en blanco
+    textAlign: 'center',
+    padding: '1rem 0', // Espaciado interno
+    position: 'relative', // Asegura que no se superponga al contenido
+    width: '100vw', // Toma todo el ancho de la ventana
+  },
+};
+
+export default Footer;
